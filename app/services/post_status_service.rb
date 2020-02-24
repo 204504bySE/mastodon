f98d7587d5b50a8bd91ce19c0b9956ee85f64961
noellabo <noel.yoshiba@gmail.com>
2020-02-24 11:36:40 +0900

    Fix boost target to be quoted if boosted

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -27,10 +27,12 @@ class PostStatusService < BaseService
     @options     = options
     @text        = @options[:text] || ''
     @in_reply_to = @options[:thread]
+    @quote_id    = @options[:quote_id]
 
     return idempotency_duplicate if idempotency_given? && idempotency_duplicate?
 
     validate_media!
+    preprocess_quote!
     preprocess_attributes!
 
     if scheduled?
@@ -57,6 +59,13 @@ class PostStatusService < BaseService
     @scheduled_at = nil if scheduled_in_the_past?
   rescue ArgumentError
     raise ActiveRecord::RecordInvalid
+  end
+
+  def preprocess_quote!
+    if @quote_id.present?
+      quote = Status.find(@quote_id)
+      @quote_id = quote.reblog_of_id.to_s if quote.reblog?
+    end
   end
 
   def process_status!
@@ -169,7 +178,7 @@ class PostStatusService < BaseService
       language: valid_locale_cascade(@options[:language], @account.user&.preferred_posting_language, I18n.default_locale),
       application: @options[:application],
       rate_limit: @options[:with_rate_limit],
-      quote_id: @options[:quote_id],
+      quote_id: @quote_id,
     }.compact
   end
 
