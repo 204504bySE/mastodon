5ac96a613d796491d080a4cb7ed30165289e6986
aries <aries@asterism.xyz>
2021-10-01 17:05:17 +0900

    Fix sort

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -35,9 +35,7 @@ class SearchService < BaseService
   end
 
   def perform_statuses_search!
-    definition = parsed_query.apply(StatusesIndex.filter(term: { searchable_by: @account.id }))
-
-    definition = definition.order(created_at: { order: 'desc' })
+    definition = parsed_query.apply(StatusesIndex.filter(term: { searchable_by: @account.id }).order(id: { order: 'desc' }))
 
     if @options[:account_id].present?
       definition = definition.filter(term: { account_id: @options[:account_id] })
