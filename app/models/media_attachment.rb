1934426998d454f1bcfe4770e9f5ac9f7aeddefb
204504bySE <204504byse@204504byse.info>
2023-08-31 08:18:07 +0900

    画像と動画のファイルサイズ制限を緩和 動画の長さ制限を解除

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -38,8 +38,8 @@ class MediaAttachment < ApplicationRecord
 
   MAX_DESCRIPTION_LENGTH = 1_500
 
-  IMAGE_LIMIT = 10.megabytes
-  VIDEO_LIMIT = 40.megabytes
+  IMAGE_LIMIT = 40.megabytes
+  VIDEO_LIMIT = 1000.megabytes
 
   MAX_VIDEO_MATRIX_LIMIT = 2_304_000 # 1920x1200px
   MAX_VIDEO_FRAME_RATE   = 60
@@ -105,7 +105,7 @@ class MediaAttachment < ApplicationRecord
         'maxrate' => '1300K',
         'bufsize' => '1300K',
         'b:v' => '1300K',
-        'frames:v' => 60 * 60 * 3,
+        #'frames:v' => 60 * 60 * 3,
         'qpmin' => '18',
         'crf' => 22,
         'map_metadata' => '-1',
